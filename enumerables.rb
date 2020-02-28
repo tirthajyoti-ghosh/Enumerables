c0f47a822fe99441a0ae270136b86af97e1804e2
Tirthajyoti Ghosh <itirthahere@gmail.com>
2020-02-28 20:05:33 +0530

    added #my_select

--- enumerables.rb
+++ enumerables.rb
@@ -26,4 +26,19 @@ module Enumerable
 
     self
   end
+
+  def my_select
+    selected_array = []
+
+    i = 0
+    while i < size
+      return to_enum unless block_given?
+
+      selected_array << self[i] if yield(self[i])
+
+      i += 1
+    end
+
+    selected_array
+  end
 end
