19bc4bfc78d6fe2bd620713423579b179b0373ef
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-03-01 23:20:14 +0530

    added #my_inject and #my_map

--- enumerables.rb
+++ enumerables.rb
@@ -1,44 +1,35 @@
 module Enumerable
   def my_each()
     i = 0
-
     while i < size
       return to_enum unless block_given?
 
       yield(self[i])
-
       i += 1
     end
-
     self
   end
 
   def my_each_with_index
     i = 0
-
     while i < size
       return to_enum unless block_given?
 
       yield(self[i], i)
-
       i += 1
     end
-
     self
   end
 
   def my_select
     selected_array = []
-
     i = 0
     while i < size
       return to_enum unless block_given?
 
       selected_array << self[i] if yield(self[i])
-
       i += 1
     end
-
     selected_array
   end
 
@@ -46,7 +37,6 @@ module Enumerable
     i = 0
     while i < size
       return false if block_given? && !yield(self[i])
-
       return false if self[i] == false || self[i].nil?
 
       i += 1
@@ -58,7 +48,6 @@ module Enumerable
     i = 0
     while i < size
       return true if block_given? && yield(self[i])
-
       return false if self[i] == false || self[i].nil?
 
       i += 1
@@ -70,7 +59,6 @@ module Enumerable
     i = 0
     while i < size
       return false if block_given? && yield(self[i])
-
       return false if self[i] == false || self[i].nil?
 
       i += 1
@@ -85,10 +73,43 @@ module Enumerable
     i = 0
     while i < size
       count += 1 if yield(self[i])
-
       i += 1
     end
-
     count
+  end
+
+  def my_inject(param1 = nil, param2 = nil)
+    array = to_a
+    accumulator = array[0]
+    i = 1
+
+    accumulator = yield(param1, accumulator) if block_given? && !param1.nil?
+    while i < array.size
+      accumulator = if block_given?
+                      yield(accumulator, array[i])
+                    elsif param2.nil?
+                      accumulator.send(param1, array[i])
+                    else
+                      accumulator.send(param2, array[i]).send(param2, param1)
+                    end
+      i += 1
+    end
+    accumulator
+  end
+
+  def my_map(&proc)
+    return to_enum unless block_given?
+
+    array = to_a
+    mapped_array = []
+    i = 0
+
+    if !proc.nil? && block_given?
+      while i < array.size
+        mapped_array << proc.call(array[i])
+        i += 1
+      end
+    end
+    mapped_array
   end
 end
