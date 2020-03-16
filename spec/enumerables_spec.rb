a315d38a4e35799a1d08ad707cdcdd7b82a37ab1
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-03-16 20:42:02 +0530

    added test for #my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -28,6 +28,28 @@ RSpec.describe Enumerable do
   end
 
   describe '#my_each_with_index' do
-    
+    it 'executes the block for each element and its index of the Array its called on' do
+      hash = {}
+      %w[cat dog wombat].my_each_with_index { |item, index| hash[item] = index }
+      expect(hash).to eql({"cat"=>0, "dog"=>1, "wombat"=>2})
+    end
+
+    it 'executes the block for each element of the Range its called on' do
+      array = []
+      (1..5).my_each_with_index { |item, index| array << (item + index) }
+      expect(array).to eql([1, 3, 5, 7, 9])
+    end
+
+    it 'returns the Array it was called upon' do
+      expect([1,2,3,4].my_each_with_index { |i| i * 2 }).to eql([1,2,3,4])
+    end
+
+    it 'returns the Range it was called upon' do
+      expect((1..5).my_each_with_index { |i| i * 2 }).to eql((1..5))
+    end
+
+    it 'returns Enumerator if no block is given' do
+      expect([6, 7, 8, 9].my_each_with_index.class).to eql(Enumerator)
+    end
   end
 end
