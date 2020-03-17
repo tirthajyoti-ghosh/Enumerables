3a94e1df87653bceb72119819c47303f592bc008
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-03-17 11:48:27 +0530

    added test for #my_any?

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -72,10 +72,6 @@ RSpec.describe Enumerable do
       expect(%w[ant bear cat].my_all? { |word| word.length >= 4 }).to eql(false)
     end
 
-    it 'returns true if condition in block is not true for all the elements in the array' do
-      expect(%w[ant bear cat].my_all? { |word| word.length >= 4 }).to eql(false)
-    end
-
     it 'returns true if the array is empty' do
       expect([].my_all?).to eql(true)
     end
@@ -144,6 +140,88 @@ RSpec.describe Enumerable do
         
         it 'returns false if the Class in the parameter is not the Class of all the elements in array' do
           expect(["1", "5i", 5.67, "true"].my_all?(String)).to eql(false)
+        end
+      end
+    end
+  end
+
+  describe "#my_any?" do
+    it 'returns true if condition in block is true for any one of the elements in the array' do
+      expect(%w[ant bear cat].my_any? { |word| word.length >= 4 }).to eql(true)
+    end
+
+    it 'returns false if condition in block is not true for any of the elements in the array' do
+      expect(%w[ant bear cat].my_any? { |word| word.length >= 5 }).to eql(false)
+    end
+
+    it 'returns false if the array is empty' do
+      expect([].my_any?).to eql(false)
+    end
+
+    describe "when no parameter and no block given" do
+      it 'returns true if any one of the element is not nil/false in array' do
+        expect([nil, false, 9].my_any?).to eql(true)
+      end
+      
+      it 'returns false if any all of the element is nil/false in array' do
+        expect([nil, false, nil].my_any?).to eql(false)
+      end
+    end
+
+    describe "when one parameter is present and no block given" do
+      describe "when parameter is a Numeric" do
+        it 'returns true if the Integer in the parameter is equal to any one of the elements in array' do
+          expect([2, 1, 6, 7, 4, 8, 10].my_any?(7)).to eql(true)
+        end
+
+        it 'returns false if the Integer in the parameter is not equal to any one of the elements in array' do
+          expect([2, 1, 6, 7, 4, 8, 10].my_any?(5)).to eql(false)
+        end
+        
+        it 'returns true if the Float in the parameter is equal to any one of the elements in array' do
+          expect([3, 56.3, 32.3, 323, 58.1].my_any?(32.3)).to eql(true)
+        end
+        
+        it 'returns false if the Float in the parameter is not equal to any one of the elements in array' do
+          expect([32.5, 32.7, 31, 42.3, 37].my_any?(32.3)).to eql(false)
+        end
+      end
+      
+      describe "when parameter is Pattern" do
+        it 'returns true if the Pattern in the parameter matches with any one of the elements in array' do
+          expect(%w[cat fruit bat].my_any?('cat')).to eql(true)
+        end
+        
+        it 'returns false if the Pattern in the parameter does not match with any one of the elements in array' do
+          expect(%w[ant bear cat].my_any?('bread')).to eql(false)
+        end
+      end
+      
+      describe "when parameter is Regexp" do
+        it 'returns true if the Regexp in the parameter matches with any one of the elements in array' do
+          expect(%w[ant bear cat].my_any?(/a/)).to eql(true)
+        end
+        
+        it 'returns false if the Regexp in the parameter does not match with any one of the elements in array' do
+          expect(%w[ant bear cat].my_any?(/d/)).to eql(false)
+        end
+      end
+      
+      describe "when parameter is Class/Superclass" do
+        it 'returns true if the Class in the parameter is a Superclass of any one of the elements in array' do
+          expect([1, 5i, 5.67].my_any?(Numeric)).to eql(true)
+        end
+        
+        it 'returns false if the Class in the parameter is not a Superclass of any one of the elements in array' do
+          expect(["1", Integer, [1, 2, 3], true].my_any?(Numeric)).to eql(false)
+        end
+        
+        it 'returns true if the Class in the parameter is the Class of any one of the elements in array' do
+          expect([2, 1, 6, 7, 4, 8, 10].my_any?(Integer)).to eql(true)
+        end
+        
+        it 'returns false if the Class in the parameter is not the Class of any one of the elements in array' do
+          expect([1, 5i, 5.67, true].my_any?(String)).to eql(false)
         end
       end
     end
